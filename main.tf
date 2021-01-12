a0b587cf2548a1225199087caa1e10e27d27d0cb
rb-nr <rb-nr@users.noreply.github.com>
2021-01-12 16:34:40 -0500

    Forgot to tag target groups

--- main.tf
+++ main.tf
@@ -159,6 +159,10 @@ resource aws_lb_target_group "target443" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
+
+  tags = merge(var.tags, {
+    Provider = "BanyanOps"
+  })
 }
 
 resource aws_lb_listener "listener443" {
@@ -185,6 +189,10 @@ resource aws_lb_target_group "target80" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
+
+  tags = merge(var.tags, {
+    Provider = "BanyanOps"
+  })
 }
 
 resource aws_lb_listener "listener80" {
@@ -211,6 +219,10 @@ resource aws_lb_target_group "target8443" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
+
+  tags = merge(var.tags, {
+    Provider = "BanyanOps"
+  })
 }
 
 resource aws_lb_listener "listener8443" {
