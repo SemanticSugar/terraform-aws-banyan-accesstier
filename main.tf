bf386bd80b7c26f2cf310b7ffbddbfc4e05765e9
Danny Fowler <danny.fowler@nextroll.com>
2025-07-11 10:42:41 -0700

    Add variables for max_instances, desired_instances, disable_scale_in, and AZ distribution
    - Add max_instances variable to override hardcoded max_size=10
    - Add desired_instances variable to set different desired capacity from min_instances
    - Add disable_scale_in variable for autoscaling policy configuration
    - Add availability_zone_distribution support with configurable strategy
    - Maintains backward compatibility with defaults

---
+++ main.tf
@@ -0,0 +1,298 @@
+locals {
+  tags = merge(var.tags, {
+    Provider = "BanyanOps"
+  })
+
+  asg_tags = merge(local.tags, {
+    Name = "${var.site_name}-BanyanHost"
+  })
+}
+
+data aws_ami "default_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = [var.default_ami_name]
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${var.name_prefix}-accesstier-sg"
+  description = "Elastic Access Tier ingress traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Web traffic"
+  }
+
+  dynamic "ingress" {
+    for_each = var.redirect_http_to_https ? [true] : []
+    content {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Redirect to 443"
+    }
+  }
+
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow for web traffic"
+  }
+
+  ingress {
+    from_port   = 9998
+    to_port     = 9998
+    protocol    = "tcp"
+    cidr_blocks = var.healthcheck_cidrs
+    description = "Healthcheck"
+  }
+
+  ingress {
+    from_port   = 2222
+    to_port     = 2222
+    protocol    = "tcp"
+    cidr_blocks = var.management_cidrs
+    description = "Management"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow egress to everything"
+  }
+
+  tags = merge(local.tags, var.security_group_tags)
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name                      = "${var.name_prefix}-accesstier-asg"
+  launch_configuration      = aws_launch_configuration.conf.name
+  max_size                  = var.max_instances
+  min_size                  = var.min_instances
+  desired_capacity          = var.desired_instances != null ? var.desired_instances : var.min_instances
+  vpc_zone_identifier       = var.private_subnet_ids
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  target_group_arns         = compact([join("", aws_lb_target_group.target80.*.arn), aws_lb_target_group.target443.arn, aws_lb_target_group.target8443.arn])
+  max_instance_lifetime     = var.max_instance_lifetime
+
+  dynamic "availability_zone_distribution" {
+    for_each = var.enable_availability_zone_distribution ? [1] : []
+    content {
+      capacity_distribution_strategy = var.capacity_distribution_strategy
+    }
+  }
+
+  dynamic "tag" {
+    # do another merge for application specific tags if need-be
+    for_each = merge(local.asg_tags, var.autoscaling_group_tags)
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+}
+
+resource "aws_launch_configuration" "conf" {
+  name_prefix     = "${var.name_prefix}-accesstier-conf-"
+  image_id        = var.ami_id != "" ? var.ami_id : data.aws_ami.default_ami.id
+  instance_type   = var.instance_type
+  key_name        = var.ssh_key_name
+  security_groups = [aws_security_group.sg.id]
+  ebs_optimized   = true
+
+  iam_instance_profile = var.iam_instance_profile
+
+  ephemeral_block_device {
+    device_name  = "/dev/sdc"
+    virtual_name = "ephemeral0"
+  }
+
+  metadata_options {
+    http_endpoint               = var.http_endpoint_imds_v2
+    http_tokens                 = var.http_tokens_imds_v2
+    http_put_response_hop_limit = var.http_hop_limit_imds_v2
+  }
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  user_data = join("", concat([
+    "#!/bin/bash -ex\n",
+    # increase file handle limits
+    "echo '* soft nofile 100000' >> /etc/security/limits.d/banyan.conf\n",
+    "echo '* hard nofile 100000' >> /etc/security/limits.d/banyan.conf\n",
+    "echo 'fs.file-max = 100000' >> /etc/sysctl.d/90-banyan.conf\n",
+    "sysctl -w fs.file-max=100000\n",
+    # increase conntrack hashtable limits
+    "echo 'options nf_conntrack hashsize=65536' >> /etc/modprobe.d/banyan.conf\n",
+    "modprobe nf_conntrack\n",
+    "echo '65536' > /proc/sys/net/netfilter/nf_conntrack_buckets\n",
+    "echo '262144' > /proc/sys/net/netfilter/nf_conntrack_max\n",
+    # install dogstatsd (if requested)
+    var.datadog_api_key != null ? "curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh | DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=${var.datadog_api_key} DD_SITE=datadoghq.com bash -v\n" : "",
+    # install prerequisites and Banyan netagent
+    "yum update -y\n",
+    "yum install -y jq tar gzip curl sed python3\n",
+    "pip3 install --upgrade pip\n",
+    "/usr/local/bin/pip3 install pybanyan\n", # previous line changes /bin/pip3 to /usr/local/bin which is not in the path
+    "rpm --import https://www.banyanops.com/onramp/repo/RPM-GPG-KEY-banyan\n",
+    "yum-config-manager --add-repo https://www.banyanops.com/onramp/repo\n",
+    "while [ -f /var/run/yum.pid ]; do sleep 1; done\n",
+    "yum install -y ${var.package_name} \n",
+    # configure and start netagent
+    "cd /opt/banyan-packages\n",
+    "BANYAN_ACCESS_TIER=true ",
+    "BANYAN_REDIRECT_TO_HTTPS=${var.redirect_http_to_https} ",
+    "BANYAN_SITE_NAME=${var.site_name} ",
+    "BANYAN_SITE_ADDRESS=${aws_alb.nlb.dns_name} ",
+    "BANYAN_SITE_DOMAIN_NAMES=", join(",", var.site_domain_names), " ",
+    "BANYAN_SITE_AUTOSCALE=true ",
+    "BANYAN_API=${var.api_server} ",
+    "BANYAN_HOST_TAGS=", join(",", [for k, v in var.host_tags : format("%s=%s", k, v)]), " ",
+    "BANYAN_ACCESS_EVENT_CREDITS_LIMITING=${var.rate_limiting.enabled} ",
+    "BANYAN_ACCESS_EVENT_CREDITS_MAX=${var.rate_limiting.max_credits} ",
+    "BANYAN_ACCESS_EVENT_CREDITS_INTERVAL=${var.rate_limiting.interval} ",
+    "BANYAN_ACCESS_EVENT_CREDITS_PER_INTERVAL=${var.rate_limiting.credits_per_interval} ",
+    "BANYAN_ACCESS_EVENT_KEY_LIMITING=${var.rate_limiting.enable_by_key} ",
+    "BANYAN_ACCESS_EVENT_KEY_EXPIRATION=${var.rate_limiting.key_lifetime} ",
+    "BANYAN_GROUPS_BY_USERINFO=${var.groups_by_userinfo} ",
+    var.datadog_api_key != null ? "BANYAN_STATSD=true BANYAN_STATSD_ADDRESS=127.0.0.1:8125 " : "",
+    "./install ${var.refresh_token} ${var.cluster_name} \n",
+    "echo 'Port 2222' >> /etc/ssh/sshd_config && /bin/systemctl restart sshd.service\n",
+  ], var.custom_user_data))
+}
+
+resource "aws_alb" "nlb" {
+  name                             = "${var.name_prefix}-nlb"
+  load_balancer_type               = "network"
+  internal                         = false
+  subnets                          = var.public_subnet_ids
+  enable_cross_zone_load_balancing = var.cross_zone_enabled
+
+  tags = merge(local.tags, var.lb_tags)
+}
+
+resource "aws_lb_target_group" "target443" {
+  name     = "${var.name_prefix}-tg-443"
+  vpc_id   = var.vpc_id
+  port     = 443
+  protocol = "TCP"
+  stickiness {
+      enabled = var.sticky_sessions
+      type = "source_ip"
+  }
+  health_check {
+    port                = 9998
+    protocol            = "HTTP"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = merge(local.tags, var.target_group_tags)
+}
+
+resource "aws_lb_listener" "listener443" {
+  load_balancer_arn = aws_alb.nlb.arn
+  port              = 443
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target443.arn
+  }
+}
+
+resource "aws_lb_target_group" "target80" {
+  count = var.redirect_http_to_https ? 1 : 0
+
+  name     = "${var.name_prefix}-tg-80"
+  vpc_id   = var.vpc_id
+  port     = 80
+  protocol = "TCP"
+  stickiness {
+      enabled = var.sticky_sessions
+      type = "source_ip"
+  }
+  health_check {
+    port                = 9998
+    protocol            = "HTTP"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = merge(local.tags, var.target_group_tags)
+}
+
+resource "aws_lb_listener" "listener80" {
+  count = var.redirect_http_to_https ? 1 : 0
+
+  load_balancer_arn = aws_alb.nlb.arn
+  port              = 80
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target80[0].arn
+  }
+}
+
+resource "aws_lb_target_group" "target8443" {
+  name     = "${var.name_prefix}-tg-8443"
+  vpc_id   = var.vpc_id
+  port     = 8443
+  protocol = "TCP"
+  stickiness {
+      enabled = var.sticky_sessions
+      type = "source_ip"
+  }
+  health_check {
+    port                = 9998
+    protocol            = "HTTP"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = merge(local.tags, var.target_group_tags)
+}
+
+resource "aws_lb_listener" "listener8443" {
+  load_balancer_arn = aws_alb.nlb.arn
+  port              = 8443
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target8443.arn
+  }
+}
+
+resource "aws_autoscaling_policy" "cpu_policy" {
+  name                   = "${var.name_prefix}-cpu-scaling-policy"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  policy_type            = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value     = 80
+    disable_scale_in = var.disable_scale_in
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,246 @@
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC in which to create the Access Tier"
+}
+
+variable "healthcheck_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks to allow health check connections from (recommended to use the VPC CIDR range)"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "management_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks to allow SSH connections from"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "public_subnet_ids" {
+  type        = list(string)
+  description = "IDs of the subnets where the load balancer should create endpoints"
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "IDs of the subnets where the Access Tier should create instances"
+}
+
+variable "package_name" {
+  type        = string
+  description = "Override to use a specific version of netagent (e.g. `banyan-netagent-1.5.0`)"
+  default     = "banyan-netagent"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type to use when creating Access Tier instances"
+  default     = "t3.large"
+}
+
+variable "site_name" {
+  type        = string
+  description = "Name to use when registering this Access Tier with the console"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of an existing Shield cluster to register this Access Tier with"
+}
+
+variable "refresh_token" {
+  type        = string
+  description = "API token generated from the Banyan console"
+}
+
+variable "site_domain_names" {
+  type        = list(string)
+  description = "List of aliases or CNAMEs that will direct traffic to this Access Tier"
+}
+
+variable "api_server" {
+  type        = string
+  description = "URL to the Banyan API server"
+  default     = "https://net.banyanops.com/api/v1"
+}
+
+variable "ssh_key_name" {
+  type        = string
+  description = "Name of an SSH key stored in AWS to allow management access"
+  default     = ""
+}
+
+variable "ami_id" {
+  type        = string
+  description = "ID of a custom AMI to use when creating Access Tier instances (leave blank to use default)"
+  default     = ""
+}
+
+variable "default_ami_name" {
+  type        = string
+  description = "If no AMI ID is supplied, use the most recent AMI from this project"
+  default     = "amzn2-ami-hvm-2.0.*-x86_64-ebs"
+}
+
+variable "cross_zone_enabled" {
+  type        = bool
+  description = "Allow load balancer to distribute traffic to other zones"
+  default     = true
+}
+
+variable "min_instances" {
+  type        = number
+  description = "Minimum number of Access Tier instances to keep alive"
+  default     = 2
+}
+
+variable "custom_user_data" {
+  type        = list(string)
+  description = "Custom commands to append to the launch configuration initialization script."
+  default     = []
+}
+
+variable "redirect_http_to_https" {
+  type        = bool
+  description = "If true, requests to the AccessTier on port 80 will be redirected to port 443"
+  default     = false
+}
+
+variable "iam_instance_profile" {
+  type        = string
+  description = "The name attribute of the IAM instance profile to associate with launched instances."
+  default     = null
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Add tags to each resource"
+  default     = null
+}
+
+variable "security_group_tags" {
+  type        = map
+  description = "Additional tags to the security_group"
+  default     = null
+}
+
+variable "autoscaling_group_tags" {
+  type        = map
+  description = "Additional tags to the autoscaling_group"
+  default     = null
+}
+
+variable "lb_tags" {
+  type        = map
+  description = "Additional tags to the lb"
+  default     = null
+}
+
+variable "target_group_tags" {
+  type        = map
+  description = "Additional tags to each target_group"
+  default     = null
+}
+
+variable "host_tags" {
+  type        = map(any)
+  description = "Additional tags to assign to this AccessTier"
+  default     = { "type" : "access_tier" }
+}
+
+variable "groups_by_userinfo" {
+  type        = bool
+  description = "Derive groups information from userinfo endpoint"
+  default     = false
+}
+
+variable "name_prefix" {
+  type        = string
+  description = "String to be added in front of all AWS object names"
+  default     = "banyan"
+}
+
+variable "rate_limiting" {
+  type = object({
+    enabled              = bool
+    max_credits          = number
+    interval             = string
+    credits_per_interval = number
+    enable_by_key        = bool
+    key_lifetime         = string
+  })
+  description = "Rate limiting configuration for access events"
+  default = {
+    enabled              = true
+    max_credits          = 5000
+    interval             = "1m"
+    credits_per_interval = 5
+    enable_by_key        = true
+    key_lifetime         = "9m"
+  }
+}
+
+variable "max_instance_lifetime" {
+  type        = number
+  default     = null
+  description = "The maximum amount of time, in seconds, that an instance can be in service, values must be either equal to 0 or between 604800 and 31536000 seconds"
+}
+
+variable "http_endpoint_imds_v2" {
+  type        = string
+  description = "value for http_endpoint to enable imds v2 for ec2 instance"
+  default     = "enabled"
+}
+
+variable "http_tokens_imds_v2" {
+  type        = string
+  description = "value for http_tokens to enable imds v2 for ec2 instance"
+  default     = "required"
+}
+
+variable "http_hop_limit_imds_v2" {
+  type        = number
+  description = "value for http_put_response_hop_limit to enable imds v2 for ec2 instance"
+  default     = 1
+}
+
+variable "datadog_api_key" {
+    type = string
+    description = "API key for DataDog"
+    default = null
+}
+
+variable "sticky_sessions" {
+    type = bool
+    description = "Enable session stickiness for apps that require it"
+    default = false
+}
+
+variable "max_instances" {
+  type        = number
+  description = "Maximum number of Access Tier instances"
+  default     = 10
+}
+
+variable "desired_instances" {
+  type        = number
+  description = "Desired number of Access Tier instances (if not specified, uses min_instances)"
+  default     = null
+}
+
+variable "disable_scale_in" {
+  type        = bool
+  description = "Disable scale-in for the autoscaling policy"
+  default     = false
+}
+
+variable "enable_availability_zone_distribution" {
+  type        = bool
+  description = "Enable availability zone distribution strategy"
+  default     = false
+}
+
+variable "capacity_distribution_strategy" {
+  type        = string
+  description = "Capacity distribution strategy for availability zones"
+  default     = "balanced-best-effort"
+}
